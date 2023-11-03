5b8d5b996f7a894f6ce77a9b0f7168a42ac0f217
bealei <bealei@163.com>
2023-11-03 19:25:08 +0800

    Initial commit

---
+++ static/data.js
@@ -0,0 +1,57 @@
+var datajson = {
+	"name": "Demo",
+	"text": "@bealei",
+	"datas": [{
+			"name": "a1",
+			"path": "./pages/a1/index.html"
+		},
+		{
+			"name": "a2",
+			"path": "./pages/a2/index.html"
+		},
+		{
+			"name": "md5加密",
+			"path": "./pages/md5加密/index.html"
+		},
+		{
+			"name": "UI主题配色",
+			"path": "./pages/UI主题配色/index.html"
+		},
+		{
+			"name": "测试宽度",
+			"path": "./pages/测试宽度/index.html"
+		},
+		{
+			"name": "调色器",
+			"path": "./pages/调色器/index.html"
+		},
+		{
+			"name": "毫米像素转换",
+			"path": "./pages/毫米像素转换/index.html"
+		},
+		{
+			"name": "随机密码",
+			"path": "./pages/随机密码/index.html"
+		},
+		{
+			"name": "随机数",
+			"path": "./pages/随机数/index.html"
+		},
+		{
+			"name": "随机数",
+			"path": "./pages/随机数/index.html"
+		},
+		{
+			"name": "随机数",
+			"path": "./pages/随机数/index.html"
+		},
+		{
+			"name": "随机数",
+			"path": "./pages/随机数/index.html"
+		},
+		{
+			"name": "随机数",
+			"path": "./pages/随机数/index.html"
+		}
+	]
+}
